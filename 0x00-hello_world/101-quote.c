5920167bbd5fdb7387deda90086650191c37e14e
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-13 21:08:59 -0700

    Quote C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#inclde <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
- *  * main- Entry point
- *   * @void: parameter
- *    * Return: 1
- *     */
-
+ * main - Entry
+ *Return: 1
+ */
 int main(void)
 {
-	    fprintf(stderr, "and that piece of art is useful\" - \
-			    Dora Korpar, 2015-10-19\n");
-
-	        return (1);
+	writer(stderr, "and that piece of art is useful\" - 
+			Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
