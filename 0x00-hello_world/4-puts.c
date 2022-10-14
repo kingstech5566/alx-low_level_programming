68a68a9fe0729347b7a3ef15e95f495c14f23d11
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-13 17:34:22 -0700

    Puts C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
   *Return: Always 0 
   */
 int main(void)
-{ 
-         puts("\"Programming is like building a multilingual puzzle");
-         return (0);
-}    
+{
+      puts("\"Programning is like building a multilingual puzzle);
+      return (0);
+} 
