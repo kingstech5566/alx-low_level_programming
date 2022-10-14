45e7dd3e62c582fabdcbc42b338b6f216c6655f2
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-13 20:59:58 -0700

    Quote C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#inclde <stdio.h>
+/**
+ *  * main- Entry point
+ *   * @void: parameter
+ *    * Return: 1
+ *     */
+
+int main(void)
+{
+	    fprintf(stderr, "and that piece of art is useful\" - \
+			    Dora Korpar, 2015-10-19\n");
+
+	        return (1);
+}
+
