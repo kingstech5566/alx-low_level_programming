f8dd4995c0f66d10263c2ae49a12af5dbc6e207c
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-18 16:56:49 -0700

    Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Count alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int num = 0;
+
+	while (num <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	}
+	_putchar('\n');
+}
