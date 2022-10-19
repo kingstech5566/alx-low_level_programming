b866282bbde2d163b97200b0fed390bbf6d7055c
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-19 01:53:09 -0700

    Isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: the integer value it recieves
+ * Description: checks is alphabet
+ * Return: 1 if true. 0 if false
+ */
+int _isalpha(int c)
+{
+	int c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
