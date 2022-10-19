a3c986d4f4536edc7287e10dfc5acfca375e234b
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-19 02:52:21 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: n as an integer
+ * Description: print the absolute value of an integer
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
