97db3fb2ef099ec126509acf12637988705f2d70
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-19 20:36:17 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,22 @@
 #include "main.h"
 /**
- * main - check the code for ALX Students
- *
- * Return: Always 0
+ * print_to_98 - print all natural numbers from n to 98
+ * @n: prints to 98
+ * Return: void
  */
-int main(void)
+void print_to_98(int n)
 {
-	print-times_table(3);
-	_putchar('\n');
-	print-times_table(5);
-	_putchar('\n');
-	print-times_table(98);
-	_putchar('\n');
-	print-times_table(12);
-	return (0);
+	int n;
+	for (n = 0; n < 98; n++)
+	{
+		printf('\n');
+		if (n <= 98)
+		{
+			printf("%d\n, n++");
+		}
+		else
+		{
+			printf("%d\n", n);
+		}
+	}
 }
