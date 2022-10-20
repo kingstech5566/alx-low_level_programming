7ebb8fed6e1ebb86795b33aea41fb8fc41235b56
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-19 20:53:17 -0700

    Alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	int j;
+	int i;
 
-	while (1 <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
+		for (j ='a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	i++;
 }
