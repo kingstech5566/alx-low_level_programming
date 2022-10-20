083de7b9f3162d2dc0504eb2ac325a90461949dd
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-19 19:26:08 -0700

    9 Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,35 +6,26 @@
  */
 void times_table(void)
 {
-	int row, column, product, tens, ones;
+	int row, column, product;
 
 	for (row = 0; row <= 9; row++)
 	{
-		for (column = 0; column <= 9; column++)
+		_putchar('0');
+		for (column = 1; column <= 9; column++)
 		{
-			product = row * column;
-			tens = product / 10;
-			ones - product % 10;
+			_putchar(',');
+			_putchar(' ');
 
-			if (column == 0)
+			product = row * column;
+
+			if (product <= 9)
 			{
-				_putchar('0');
-			}
-			else if (product < 10)
-			{
-				_putchar(',');
-				_putchar('');
-				_putchar('');
-				_putchar(ones + '0');
+				_putchar(' ');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar('');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
+				_putchar((product / 10) + '0');
 			}
 		}
-		_putchar('\n');
 	}
 }
