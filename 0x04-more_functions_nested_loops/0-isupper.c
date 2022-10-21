793e57ad731a7c62cd9c39ceac011f0c2f7e9d9c
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-20 20:16:45 -0700

    Uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
  * int _isupper - print uppercase letter
- * Description: print Upper case class
+ * Description: print Uppercase
  * @c: char to check
  * Return: 0 or 1
  */
 int _isupper(int c)
 {
-	for (c = 'a'; c <= 'z'; c++)
+	int c;
+
+	if (c >= 'A' && c <= 'Z')
 	{
-		else if (c >= 'A' && c <= 'Z')
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
