9ce10bb17661611b73ea1bd2231126a15695aa63
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-21 23:07:58 -0700

    Prints most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,14 @@
  */
 void print_most_numbers(void)
 {
-	char c;
+	char c = 0;
 
-	for (c = '0'; c < '9'; c++)
+	while (c < 10) 
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		if (c != '2' && c != '4')
+			_putchar(c + '0');
+
+		c++;
 	}
 	_putchar('\n');
 }
