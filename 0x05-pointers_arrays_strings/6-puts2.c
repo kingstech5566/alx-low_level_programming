aa28a8b3b7b2733a5cdcc1f076183fea34ddfdd6
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-10-25 13:40:57 -0700

    Puts 2 C

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\0', i++)
+	for (; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
-			_putchat(str[i]);
-		else 
-			continue;
+			_putchar(str[i]);
+		else
+			continue
 	}
 	_putchar('\n');
 }
