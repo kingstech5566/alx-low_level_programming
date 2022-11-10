b2267da0f4644d34102a33c9b148bd0857072f1a
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-11-10 15:54:44 -0800

    Create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - create an array of size and assign char c
+ * @size: the size of array
+ * @c: the char assigned
+ * Return: pointer of array, NULL if fail
+ */
+har *create_array(unsigned int size, char c)
+{
+	char str;
+	unsigned int i;
+
+	str = malloc(sizeof(car) * size);
+
+	if (size == 0 || str == NULL)
+		return (NULL);
+	
+	for (i =0; i < size; i++)
+		str[i] = c;
+	return (str);
