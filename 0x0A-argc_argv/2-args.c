76f32b06dd14706fb042fa7c370164801be8fc94
kingstech5566 <kingsleychijioke5566@gmail.com>
2022-11-11 13:34:23 -0800

    Declaring variables

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: Cunts arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	/* Declaring variables */
+	int count = 0;
+
+	if (argc > 0)
+	{
+		/* While - print each arguments */
+		while (count < 0)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
